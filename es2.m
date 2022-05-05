1be1579ca3d0139e2cb0212caa77d04fbf7acc37
Enrico Tagliaferri - enrico.tagliaferri2@studio.unibo.it <enrico.tagliaferri2@studio.unibo.it>
2022-05-05 11:09:57 +0200

    Use bisection method

--- es2.m
+++ es2.m
@@ -1,19 +1,25 @@
 clc
 clear all
 
+format long e
+
 f = @(x) x.^3 - 8;
 Df = @(x) 3*x.^2;
 
 x0 = 1;
 tolx = 1e-10;
 tolf = tolx;
-nmax = 10;
+nmax = 100;
 [xn, xkn, itn] = newton(f, Df, x0, tolx, tolf, nmax)
 
 x1 = 3;
 [xs, xks, its] = secanti(f, x0, x1, tolx, tolf, nmax)
 
-plot(1:itn, xkn, 'ro-', 1:its, xks, 'go-')
-legend('Newton', 'Secanti')
+a = 0;
+b = 3;
+[xb, xkb, itb] = bisezione(f, a, b, tolx)
+
+plot(1:itn, xkn, 'ro-', 1:its, xks, 'go-', 1:itb, xkb, 'bo-')
+legend('Newton', 'Secanti', 'Bisezione')
 
 
